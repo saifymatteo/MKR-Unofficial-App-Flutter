aac501ad5d877060269014f06d35e138209b7f0c
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-02-14 23:46:46 +0800

    UI update

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,23 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('MyKampus Radio'),
         ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.only(bottom: 20),
+          child: FloatingActionButton(
+            onPressed: null,
+            backgroundColor: kMKRColorMain,
+            child: const Icon(Icons.play_arrow_rounded),
+          ),
+        ),
+        body: const Center(
+          child: Padding(
+            padding: EdgeInsets.all(50),
+            child: Image(
+              image: AssetImage('images/MKR-logo-small-blue.png'),
+            ),
+          ),
+        ),
         drawer: Drawer(
           shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.only(
@@ -32,85 +49,120 @@ class MyApp extends StatelessWidget {
             ),
           ),
           child: ListView(
-            children: const [
-              DrawerHeader(
-                child: Text('Drawer Header Text'),
+            children: [
+              const DrawerHeader(
+                child: Image(
+                  image: AssetImage('images/MKR-logo-long-blue.png'),
+                ),
               ),
               ListTile(
-                leading: FaIcon(FontAwesomeIcons.headphones),
-                title: Text('Listen Now!'),
+                leading: FaIcon(
+                  FontAwesomeIcons.headphones,
+                  color: kMKRColorMain,
+                ),
+                title: const Text('Listen Now!'),
               ),
-              Divider(
+              const Divider(
                 height: 20,
                 thickness: 1,
               ),
-              Padding(
+              const Padding(
                 padding: EdgeInsets.only(left: 15, top: 10, bottom: 10),
                 child: Text('Content Hangat!'),
               ),
               ListTile(
-                leading: FaIcon(FontAwesomeIcons.globeAsia),
-                title: Text('Laman Web'),
+                leading: FaIcon(
+                  FontAwesomeIcons.globeAsia,
+                  color: kMKRColorMain,
+                ),
+                title: const Text('Laman Web'),
               ),
               ListTile(
-                leading: FaIcon(FontAwesomeIcons.microphone),
-                title: Text('Show Clip'),
+                leading: Icon(
+                  Icons.mic,
+                  color: kMKRColorMain,
+                ),
+                title: const Text('Show Clip'),
               ),
               ListTile(
-                leading: FaIcon(FontAwesomeIcons.youtube),
-                title: Text('MyKampus TV'),
+                leading: FaIcon(
+                  FontAwesomeIcons.youtube,
+                  color: kMKRColorMain,
+                ),
+                title: const Text('MyKampus TV'),
               ),
-              Divider(
+              const Divider(
                 height: 20,
                 thickness: 1,
               ),
-              Padding(
+              const Padding(
                 padding: EdgeInsets.only(left: 15, top: 10, bottom: 10),
                 child: Text('Social Media'),
               ),
               ListTile(
-                leading: FaIcon(FontAwesomeIcons.facebook),
-                title: Text('Facebook'),
+                leading: FaIcon(
+                  FontAwesomeIcons.facebook,
+                  color: kMKRColorMain,
+                ),
+                title: const Text('Facebook'),
               ),
               ListTile(
-                leading: FaIcon(FontAwesomeIcons.twitter),
-                title: Text('Twitter'),
+                leading: FaIcon(
+                  FontAwesomeIcons.twitter,
+                  color: kMKRColorMain,
+                ),
+                title: const Text('Twitter'),
               ),
               ListTile(
-                leading: FaIcon(FontAwesomeIcons.instagramSquare),
-                title: Text('Instagram'),
+                leading: FaIcon(
+                  FontAwesomeIcons.instagramSquare,
+                  color: kMKRColorMain,
+                ),
+                title: const Text('Instagram'),
               ),
-              Divider(
+              const Divider(
                 height: 20,
                 thickness: 1,
               ),
-              Padding(
+              const Padding(
                 padding: EdgeInsets.only(left: 15, top: 10, bottom: 10),
                 child: Text('Hubungi Kami'),
               ),
               ListTile(
-                leading: FaIcon(FontAwesomeIcons.phone),
-                title: Text('Telephone'),
+                leading: Icon(
+                  Icons.call,
+                  color: kMKRColorMain,
+                ),
+                title: const Text('Telephone'),
               ),
               ListTile(
-                leading: FaIcon(FontAwesomeIcons.whatsappSquare),
-                title: Text('WhatsApp'),
+                leading: FaIcon(
+                  FontAwesomeIcons.whatsappSquare,
+                  color: kMKRColorMain,
+                ),
+                title: const Text('WhatsApp'),
               ),
               ListTile(
-                leading: FaIcon(FontAwesomeIcons.locationArrow),
-                title: Text('Lokasi'),
+                leading: Icon(
+                  Icons.place,
+                  color: kMKRColorMain,
+                ),
+                title: const Text('Lokasi'),
               ),
-              Divider(
+              const Divider(
                 height: 20,
                 thickness: 1,
               ),
-              Padding(
+              const Padding(
                 padding: EdgeInsets.only(left: 15, top: 10, bottom: 10),
                 child: Text('Extra'),
               ),
               ListTile(
-                leading: FaIcon(FontAwesomeIcons.toolbox),
-                title: Text('Settings'),
+                leading: Icon(
+                  Icons.settings,
+                  color: kMKRColorMain,
+                ),
+                title: const Text('Settings'),
               ),
             ],
           ),
