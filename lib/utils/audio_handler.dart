074af8097d4d88d57106aec2a118c1c778ef0fb0
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-02-27 16:07:18 +0800

    refactor Audio logic

---
+++ lib/utils/audio_handler.dart
@@ -0,0 +1,125 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:audio_service/audio_service.dart';
+import 'audio_metadata.dart';
+
+class AudioPlayerHandler extends BaseAudioHandler {
+  final audioPlayer = AudioPlayer();
+  final audioMetadata = AudioMetadata();
+  late Timer timer;
+
+  String radioURL = 'https://usa10.fastcast4u.com/voicespl';
+
+  String fileName = '';
+  String titleSong = '';
+  String artistSong = '';
+
+  late MediaItem item;
+
+  AudioPlayerHandler() {
+    // Broadcast that we're loading, and what controls are available.
+    playbackState.add(playbackState.value.copyWith(
+      controls: [MediaControl.play],
+      processingState: AudioProcessingState.loading,
+    ));
+    item = setMediaItem();
+    mediaItem.add(item);
+    periodicUpdateMetadata();
+    // Connect to the URL
+    audioPlayer.setUrl(radioURL).then((_) {
+      // Broadcast that we've finished loading
+      playbackState.add(playbackState.value.copyWith(
+        processingState: AudioProcessingState.ready,
+      ));
+    });
+  }
+
+  void getMetadata() async {
+    await audioMetadata.getURLMetadata();
+    fileName = audioMetadata.fileName;
+    debugPrint('fileName: $fileName');
+    titleSong = audioMetadata.titleSong;
+    debugPrint('titleSong: $titleSong');
+    artistSong = audioMetadata.artistSong;
+    debugPrint('artistSong: $artistSong');
+    updateNewMediaItem();
+  }
+
+  void updateNewMediaItem() {
+    item = MediaItem(
+      id: radioURL,
+      album: 'MKR Stream',
+      title: titleSong,
+      artist: artistSong,
+      artUri: Uri.parse(
+          'https://mykampusradio.com/wp-content/uploads/2022/02/MKR-logo-small-blue-e1645612000865.png'),
+    );
+    mediaItem.add(item);
+  }
+
+  void periodicUpdateMetadata() {
+    timer = Timer.periodic(const Duration(seconds: 10), (timer) {
+      getMetadata();
+    });
+  }
+
+  void destroyTimer() {
+    timer.cancel();
+  }
+
+  MediaItem setMediaItem() {
+    try {
+      MediaItem mediaItem = MediaItem(
+        id: radioURL,
+        album: 'MKR Stream',
+        title: titleSong,
+        artist: artistSong,
+        artUri: Uri.parse(
+            'https://mykampusradio.com/wp-content/uploads/2022/02/MKR-logo-small-blue-e1645612000865.png'),
+      );
+      debugPrint('mediaItem Title: $titleSong');
+      debugPrint('mediaItem Artist: $artistSong');
+      return mediaItem;
+    } catch (e) {
+      debugPrint('Error: $e');
+      return MediaItem(
+        id: radioURL,
+        album: 'MKR Stream',
+        title: 'Various Song',
+        artist: 'Various Artist',
+        artUri: Uri.parse(
+            'https://mykampusradio.com/wp-content/uploads/2022/02/MKR-logo-small-blue-e1645612000865.png'),
+      );
+    }
+  }
+
+  // Play the audio
+  @override
+  Future<void> play() async {
+    playbackState.add(playbackState.value.copyWith(
+      playing: true,
+      controls: [MediaControl.pause],
+    ));
+    await audioPlayer.play();
+  }
+
+  // Pause the audio
+  @override
+  Future<void> pause() async {
+    playbackState.add(playbackState.value.copyWith(
+      playing: false,
+      controls: [MediaControl.play],
+    ));
+    await audioPlayer.pause();
+  }
+
+  @override
+  Future<void> stop() async {
+    playbackState.add(playbackState.value.copyWith(
+      processingState: AudioProcessingState.idle,
+    ));
+    await audioPlayer.stop();
+  }
+}
---
+++ lib/utils/audio_metadata.dart
@@ -0,0 +1,46 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+
+class AudioMetadata {
+  String metaURL = 'http://usa10.fastcast4u.com:4800/statistics?json=1';
+  String fileName = '';
+  String titleSong = '';
+  String artistSong = '';
+
+  Future<void> getURLMetadata() async {
+    Response response = await get(Uri.parse(metaURL));
+
+    if (response.statusCode == 200) {
+      fileName = jsonDecode(response.body)["streams"][0]["songtitle"];
+      artistSong = getArtistName(fileName);
+      titleSong = getTitleName(fileName);
+      debugPrint('Now Playing: $fileName');
+    } else {
+      debugPrint('${response.statusCode}');
+      debugPrint('${response.reasonPhrase}');
+    }
+  }
+
+  String getArtistName(String name) {
+    String placeholder = ' - ';
+
+    if (name.contains(placeholder)) {
+      int indexOfPlaceholder = name.indexOf(placeholder);
+      return name.substring(0, indexOfPlaceholder);
+    } else {
+      return 'Various Artist';
+    }
+  }
+
+  String getTitleName(String name) {
+    String placeholder = ' - ';
+
+    if (name.contains(placeholder)) {
+      int indexOfPlaceholder = name.indexOf(placeholder);
+      return name.substring((indexOfPlaceholder + 3), (name.length));
+    } else {
+      return 'Various Song';
+    }
+  }
+}
