133dbdcf5daf1fb847ae695a7b51ea1217569951
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-02-27 17:44:08 +0800

    added Navigator

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,8 @@ import 'utils/constant.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'utils/audio_handler.dart';
 import 'dart:async';
-import 'screens/listen_now_screen.dart';
 import 'screens/main_screen.dart';
+import 'utils/screen_name.dart';
 
 late AudioHandler audioHandler;
 Future<void> main() async {
@@ -22,23 +22,12 @@ Future<void> main() async {
       // androidNotificationIcon: , //TODO: to add custom icon (monochrome) to notification bar
     ),
   );
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
-  @override
-  void dispose() {
-    audioHandler.stop();
-    super.dispose();
-  }
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+  final mainNavigatorKey = GlobalKey<NavigatorState>();
 
   @override
   Widget build(BuildContext context) {
@@ -50,11 +39,81 @@ class _MyAppState extends State<MyApp> {
           Theme.of(context).textTheme,
         ),
       ),
-      initialRoute: '/',
+      navigatorKey: mainNavigatorKey,
       routes: {
-        '/': (context) => MainScreen(audioHandler),
+        Screen.home: (context) => MainScreen(
+              audioHandler: audioHandler,
+              title: 'MyKampus Radio',
+              route: Screen.home,
+              navigatorKey: mainNavigatorKey,
+            ),
+        Screen.lamanWebScreen: (context) => MainScreen(
+              audioHandler: audioHandler,
+              title: 'Laman Web',
+              route: Screen.lamanWebScreen,
+              navigatorKey: mainNavigatorKey,
+            ),
+        Screen.showClipScreen: (context) => MainScreen(
+              audioHandler: audioHandler,
+              title: 'Show Clip',
+              route: Screen.showClipScreen,
+              navigatorKey: mainNavigatorKey,
+            ),
+        Screen.myKampusTvScreen: (context) => MainScreen(
+              audioHandler: audioHandler,
+              title: 'MyKampus TV',
+              route: Screen.myKampusTvScreen,
+              navigatorKey: mainNavigatorKey,
+            ),
+        Screen.socmedFacebookScreen: (context) => MainScreen(
+              audioHandler: audioHandler,
+              title: 'Facebook',
+              route: Screen.socmedFacebookScreen,
+              navigatorKey: mainNavigatorKey,
+            ),
+        Screen.socmedTwitterScreen: (context) => MainScreen(
+              audioHandler: audioHandler,
+              title: 'Twitter',
+              route: Screen.socmedTwitterScreen,
+              navigatorKey: mainNavigatorKey,
+            ),
+        Screen.socmedInstagramScreen: (context) => MainScreen(
+              audioHandler: audioHandler,
+              title: 'Instagram',
+              route: Screen.socmedInstagramScreen,
+              navigatorKey: mainNavigatorKey,
+            ),
+        Screen.socmedTikTokScreen: (context) => MainScreen(
+              audioHandler: audioHandler,
+              title: 'TikTok',
+              route: Screen.socmedTikTokScreen,
+              navigatorKey: mainNavigatorKey,
+            ),
+        Screen.telephoneScreen: (context) => MainScreen(
+              audioHandler: audioHandler,
+              title: 'Telephone',
+              route: Screen.telephoneScreen,
+              navigatorKey: mainNavigatorKey,
+            ),
+        Screen.whatsappScreen: (context) => MainScreen(
+              audioHandler: audioHandler,
+              title: 'WhatsApp',
+              route: Screen.whatsappScreen,
+              navigatorKey: mainNavigatorKey,
+            ),
+        Screen.lokasiScreen: (context) => MainScreen(
+              audioHandler: audioHandler,
+              title: 'Lokasi',
+              route: Screen.lokasiScreen,
+              navigatorKey: mainNavigatorKey,
+            ),
+        Screen.settingsScreen: (context) => MainScreen(
+              audioHandler: audioHandler,
+              title: 'Settings',
+              route: Screen.settingsScreen,
+              navigatorKey: mainNavigatorKey,
+            ),
       },
-      // home: MainScreen(audioHandler),
     );
   }
 }
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,23 +1,52 @@
 import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
-import 'package:mkr_flutter/screens/listen_now_screen.dart';
+
 import 'package:mkr_flutter/components/drawer.dart';
 import 'package:mkr_flutter/utils/constant.dart';
+import 'package:mkr_flutter/utils/screen_name.dart';
 
-class MainScreen extends StatelessWidget {
+import 'package:mkr_flutter/screens/laman_web_screen.dart';
+import 'package:mkr_flutter/screens/listen_now_screen.dart';
+import 'package:mkr_flutter/screens/lokasi_screen.dart';
+import 'package:mkr_flutter/screens/mykampus_tv_screen.dart';
+import 'package:mkr_flutter/screens/settings_screen.dart';
+import 'package:mkr_flutter/screens/show_clip_screen.dart';
+import 'package:mkr_flutter/screens/socmed_facebook_screen.dart';
+import 'package:mkr_flutter/screens/socmed_instagram_screen.dart';
+import 'package:mkr_flutter/screens/socmed_tiktok_screen.dart';
+import 'package:mkr_flutter/screens/socmed_twitter_screen.dart';
+import 'package:mkr_flutter/screens/telephone_screen.dart';
+import 'package:mkr_flutter/screens/whatsapp_screen.dart';
+import 'package:mkr_flutter/screens/error_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({
+    Key? key,
+    required this.audioHandler,
+    required this.title,
+    required this.route,
+    required this.navigatorKey,
+  }) : super(key: key);
+
   final AudioHandler audioHandler;
+  final String title;
+  final String route;
+  final GlobalKey<NavigatorState> navigatorKey;
 
-  const MainScreen(this.audioHandler);
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
 
+class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('MyKampus Radio'),
+        title: Text(widget.title),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: StreamBuilder<PlaybackState>(
-          stream: audioHandler.playbackState,
+          stream: widget.audioHandler.playbackState,
           builder: (context, snapshot) {
             final playing = snapshot.data?.playing ?? false;
             String isPlaying = playing ? 'Playing' : 'Pause';
@@ -26,10 +55,10 @@ class MainScreen extends StatelessWidget {
               child: FloatingActionButton.extended(
                 onPressed: () {
                   if (playing) {
-                    audioHandler.pause();
+                    widget.audioHandler.pause();
                     debugPrint('Audio is pause');
                   } else {
-                    audioHandler.play();
+                    widget.audioHandler.play();
                     debugPrint('Audio is playing');
                   }
                 },
@@ -41,8 +70,83 @@ class MainScreen extends StatelessWidget {
               ),
             );
           }),
-      body: ListenNowScreen(audioHandler),
-      drawer: const DrawerNav(),
+      drawer: DrawerNav(
+        navigatorKey: widget.navigatorKey,
+      ),
+      body: Navigator(
+        onGenerateRoute: (RouteSettings settings) {
+          WidgetBuilder builder;
+          switch (widget.route) {
+            case Screen.home:
+              builder = (BuildContext context) => listenNowScreen();
+              break;
+            case Screen.lamanWebScreen:
+              builder = (BuildContext context) => lamanWebScreen();
+              break;
+            case Screen.showClipScreen:
+              builder = (BuildContext context) => showClipScreen();
+              break;
+            case Screen.myKampusTvScreen:
+              builder = (BuildContext context) => myKampusTvScreen();
+              break;
+            case Screen.socmedFacebookScreen:
+              builder = (BuildContext context) => socmedFacebookScreen();
+              break;
+            case Screen.socmedTwitterScreen:
+              builder = (BuildContext context) => socmedTwitterScreen();
+              break;
+            case Screen.socmedInstagramScreen:
+              builder = (BuildContext context) => socmedInstagramScreen();
+              break;
+            case Screen.socmedTikTokScreen:
+              builder = (BuildContext context) => socmedTikTokScreen();
+              break;
+            case Screen.telephoneScreen:
+              builder = (BuildContext context) => telephoneScreen();
+              break;
+            case Screen.whatsappScreen:
+              builder = (BuildContext context) => whatsappScreen();
+              break;
+            case Screen.lokasiScreen:
+              builder = (BuildContext context) => lokasiScreen();
+              break;
+            case Screen.settingsScreen:
+              builder = (BuildContext context) => settingsScreen();
+              break;
+            default:
+              builder = (BuildContext context) => const ErrorScreen();
+          }
+          return MaterialPageRoute(
+            builder: builder,
+            settings: settings,
+          );
+        },
+      ),
     );
   }
+
+  Widget listenNowScreen() => ListenNowScreen(
+      audioHandler: widget.audioHandler, navigatorKey: widget.navigatorKey);
+
+  Widget lamanWebScreen() => LamanWebScreen(navigatorKey: widget.navigatorKey);
+
+  Widget showClipScreen() => ShowClipScreen(navigatorKey: widget.navigatorKey);
+
+  Widget myKampusTvScreen() => MyKampusTvScreen(navigatorKey: widget.navigatorKey);
+
+  Widget socmedFacebookScreen() => SocmedFacebookScreen(navigatorKey: widget.navigatorKey);
+
+  Widget socmedTwitterScreen() => SocmedTwitterScreen(navigatorKey: widget.navigatorKey);
+
+  Widget socmedInstagramScreen() => SocmedInstagramScreen(navigatorKey: widget.navigatorKey);
+
+  Widget socmedTikTokScreen() => SocmedTikTokScreen(navigatorKey: widget.navigatorKey);
+
+  Widget telephoneScreen() => TelephoneScreen(navigatorKey: widget.navigatorKey);
+
+  Widget whatsappScreen() => WhatsappScreen(navigatorKey: widget.navigatorKey);
+
+  Widget lokasiScreen() => LokasiScreen(navigatorKey: widget.navigatorKey);
+
+  Widget settingsScreen() => SettingScreen(navigatorKey: widget.navigatorKey);
 }
