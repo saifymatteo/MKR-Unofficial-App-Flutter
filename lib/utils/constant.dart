c06d27142a58753ff754b492430f622ec7791b1f
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-03-01 00:12:19 +0800

    added new color

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -3,5 +3,6 @@ import 'package:mkr_flutter/utils/helper.dart';
 import 'package:flutter/material.dart';
 
 MaterialColor kMKRColorMain = createMaterialColor(const Color(0xFF6097D0));
+MaterialColor kMKRColorMainDark = createMaterialColor(const Color(0xFF58728F));
 
 TextStyle kDrawerText = GoogleFonts.poppins();
