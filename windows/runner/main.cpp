45304807f78f856d015d26b93926c10102c32875
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-03-20 14:12:20 +0800

    changed Windows position

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -25,7 +25,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
+  Win32Window::Point origin(100, 100);
   Win32Window::Size size(360, 640);
   if (!window.CreateAndShow(L"MyKampus Radio Unofficial", origin, size)) {
     return EXIT_FAILURE;
